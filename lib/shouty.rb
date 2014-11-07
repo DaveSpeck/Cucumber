235c4867150874ef05aa9530579849ebc0d009c2
Dave Speck <davewspeck@gmail.com>
2014-11-07 06:09:16 -0500

    Update shouty.rb

--- lib/shouty.rb
+++ lib/shouty.rb
@@ -1,5 +1,10 @@
 # This is where we'll write the code of our Shouty app
 
 class Person
-	attr_accessor :location
+	attr_accessor :location, :last_heard_message
+	
+	def shouts(message)
+	end
 end
+
+
