c8b1cf13320d25a8f4628d9af9c12cb7351bea47
Dave Speck <davewspeck@gmail.com>
2014-11-07 06:12:46 -0500

    Update shout_steps.rb

--- features/step_definitions/shout_steps.rb
+++ features/step_definitions/shout_steps.rb
@@ -1,15 +1,21 @@
 require 'shouty'
 
+Before do
+  shoutbox = Shoutbox.new
+  @sean = Person.new(shoutbox)
+  @lucy = Person.new(shoutbox)
+end
+
+
 Given(/^that Lucy is (\d+) feet away from Sean$/) do |distance|
-  @lucy = Person.new
-  @sean = Person.new
-  @lucy.location = distance
   @sean.location = 0
+  @lucy.location = distance.to_i
 end
 
 When(/^Sean shouts "(.*?)"$/) do |message|
-  @sean.shouts(message)
+  @sean.shout(message)
   @seans_message = message
+  puts @seans_message
 end
 
 Then(/^Lucy does not hear Sean's shout$/) do
@@ -17,3 +23,16 @@ Then(/^Lucy does not hear Sean's shout$/) do
   	raise "Lucy heard Sean's message (but we expected her not to)"
   end
 end
+
+Then(/^Lucy hears Sean's shout$/) do
+  if @lucy.last_heard_message != @seans_message
+  	raise "Lucy did not hear Sean's message (but we expect her to)"
+  end
+end
+
+Then(/^Sean should not hear himself$/) do
+  if @sean.last_heard_message == @seans_message
+  	raise "Sean heard his own message (but we expected him not to)"
+  end
+end
+
